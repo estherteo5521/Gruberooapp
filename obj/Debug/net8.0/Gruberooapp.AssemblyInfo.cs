c4ccaa37a265c6f512118301743a3525dc295e99
estherteo5521 <estherteo925@gmail.com>
2026-02-09 18:57:04 +0800

    update

--- obj/Debug/net8.0/Gruberooapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gruberooapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gruberooapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06e6d326bd4415a96af3560d6b534fd00b2bf923")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a2ff35d11d63ff083c88a4e438b4fcedb1b6b98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gruberooapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gruberooapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
